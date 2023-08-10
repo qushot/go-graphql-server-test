4e0458c1b045a9e8099101d12ea9fa004ef228a8
qushot <yuhtaro.moto@gmail.com>
2023-08-11 01:56:42 +0900

    format: 01_init.sql

--- docker/initdb/mysql/01_init.sql
+++ docker/initdb/mysql/01_init.sql
@@ -2,13 +2,17 @@ DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb DEFAULT CHARACTER SET utf8mb4;
 USE testdb;
 
+
 CREATE TABLE testuser (
-    id int,
-    name varchar(10),
-    address varchar(10)
+    id int
+    , name varchar(10)
+    , address varchar(10)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='テスト用テーブル';
 
+
 INSERT INTO testuser
-VALUES (1, 'Yamada', 'Tokyo'),
-       (2, 'Tanaka', 'Kanagawa'),
-       (3, 'Sato', 'Chiba');
+VALUES
+    (1, 'Yamada', 'Tokyo')
+    , (2, 'Tanaka', 'Kanagawa')
+    , (3, 'Sato', 'Chiba')
+;
