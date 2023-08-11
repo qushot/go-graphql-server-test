cf8aee46e550e9ee326fdb5e956f560c7b1f88d0
qushot <yuhtaro.moto@gmail.com>
2023-08-11 20:53:53 +0900

    chore(todo.go): fix log message

--- repository/todo.go
+++ repository/todo.go
@@ -19,6 +19,8 @@ func NewTodo(c *Client) *Todo {
 }
 
 func (t *Todo) GetAll() ([]model.Todo, error) {
+	log.Printf("START: repository#Todo#GetAll")
+
 	rows, err := t.c.Query("SELECT id, text, done, user_id FROM todo LOCK IN SHARE MODE")
 	if err != nil {
 		log.Fatalf("'todo' t.c.Query error: %v", err)
@@ -39,13 +41,15 @@ func (t *Todo) GetAll() ([]model.Todo, error) {
 		log.Fatalf("'todo' rows.Err() error: %v", err)
 	}
 
-	log.Printf("DONE: repository#Todo#GetAll")
 	log.Printf("count: %v", len(ts))
+	log.Printf("DONE: repository#Todo#GetAll")
 
 	return ts, nil
 }
 
 func (t *Todo) Create(input model.NewTodo) (*model.Todo, error) {
+	log.Printf("START: repository#Todo#Create")
+
 	stmt, err := t.c.Prepare("INSERT INTO todo (id, text, user_id) VALUES (?, ?, ?)")
 	if err != nil {
 		log.Fatalf("'todo' t.c.Prepare error: %v", err)
@@ -65,8 +69,8 @@ func (t *Todo) Create(input model.NewTodo) (*model.Todo, error) {
 		UserID: input.UserID,
 	}
 
-	log.Printf("DONE: repository#Todo#Create")
 	log.Printf("m: %+v", m)
+	log.Printf("DONE: repository#Todo#Create")
 
 	return &m, nil
 }
